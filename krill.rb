82196a13bc8a987ed4c260076706d1b0072660c2
Jake Runzer <jakerunzer@gmail.com>
2019-05-18 01:12:59 +0100

    fix sha256 hash

--- krill.rb
+++ krill.rb
@@ -2,7 +2,7 @@ class Krill < Formula
   desc "Dynamic, immutable, curried, and minimal programming language."
   homepage "https://krill.jakerunzer.com"
   url "https://github.com/coffee-cup/krill/releases/download/v1.0.0/krill-v1.0.0-Darwin-x86_64"
-  sha256 "4ee2c9fcc648469d6ff2baa414ebc0c57c70805fed8172b21a7c4e32164a0ea7"
+  sha256 "ada840f8b0746ede019aa4164fbe551217afcd8bd3bfe4e51a48a6c9552192b3"
   version "1.0.0"
 
   bottle :unneeded
