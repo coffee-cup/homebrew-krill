c45fd189ebee82fe6fd12a863010795edf7cee7a
Jake Runzer <jakerunzer@gmail.com>
2019-05-18 01:30:00 +0100

    update url to executable included in repo

--- krill.rb
+++ krill.rb
@@ -1,7 +1,7 @@
 class Krill < Formula
   desc "Dynamic, immutable, curried, and minimal programming language."
   homepage "https://krill.jakerunzer.com"
-  url "https://github.com/coffee-cup/krill/releases/download/v1.0.0/krill-v1.0.0-Darwin-x86_64"
+  url "https://raw.githubusercontent.com/coffee-cup/homebrew-krill/master/krill"
   sha256 "ada840f8b0746ede019aa4164fbe551217afcd8bd3bfe4e51a48a6c9552192b3"
   version "1.0.0"
 
